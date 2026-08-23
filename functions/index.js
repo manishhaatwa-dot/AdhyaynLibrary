fe5d37f76705a6a1f1a1628d763306af06ebc982
manishhaatwa-dot <manishhaatwa@gmail.com>
2026-08-24 00:54:37 +0530

    Implement demo admin setup function

---
+++ functions/index.js
@@ -0,0 +1,205 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+const db = admin.firestore();
+
+
+/* ==========================================================================
+   DEMO ADMIN SETUP
+   ========================================================================== */
+
+exports.setupDemoAdmin = functions.https.onCall(
+    async (data, context) => {
+
+        const demoEmail =
+            "demo@adhyayn.com";
+
+        const demoPassword =
+            "Demo@12345";
+
+        const libraryId =
+            "ADHYAYN_MAIN";
+
+
+        try {
+
+            /*
+             * ----------------------------------------------------------
+             * CHECK IF DEMO AUTH ACCOUNT ALREADY EXISTS
+             * ----------------------------------------------------------
+             */
+
+            let user;
+
+
+            try {
+
+                user =
+                    await admin
+                        .auth()
+                        .getUserByEmail(
+                            demoEmail
+                        );
+
+            }
+            catch (error) {
+
+                if (
+                    error.code ===
+                    "auth/user-not-found"
+                ) {
+
+                    user =
+                        await admin
+                            .auth()
+                            .createUser({
+
+                                email:
+                                    demoEmail,
+
+                                password:
+                                    demoPassword,
+
+                                emailVerified:
+                                    true
+
+                            });
+
+                }
+                else {
+
+                    throw error;
+
+                }
+
+            }
+
+
+            /*
+             * ----------------------------------------------------------
+             * LIBRARY DOCUMENT
+             * ----------------------------------------------------------
+             */
+
+            await db
+                .collection(
+                    "adhyayn_libraries"
+                )
+                .doc(
+                    libraryId
+                )
+                .set({
+
+                    libraryName:
+                        "Adhyayn Library",
+
+                    libraryId:
+                        libraryId,
+
+                    ownerUID:
+                        user.uid,
+
+                    ownerEmail:
+                        demoEmail,
+
+                    updatedAt:
+                        admin.firestore
+                            .FieldValue
+                            .serverTimestamp()
+
+                }, {
+
+                    merge:
+                        true
+
+                });
+
+
+            /*
+             * ----------------------------------------------------------
+             * ADMIN PROFILE
+             * ----------------------------------------------------------
+             */
+
+            await db
+                .collection(
+                    "adhyayn_libraries"
+                )
+                .doc(
+                    libraryId
+                )
+                .collection(
+                    "admins"
+                )
+                .doc(
+                    user.uid
+                )
+                .set({
+
+                    uid:
+                        user.uid,
+
+                    email:
+                        demoEmail,
+
+                    role:
+                        "admin",
+
+                    libraryId:
+                        libraryId,
+
+                    libraryName:
+                        "Adhyayn Library",
+
+                    createdAt:
+                        admin.firestore
+                            .FieldValue
+                            .serverTimestamp()
+
+                }, {
+
+                    merge:
+                        true
+
+                });
+
+
+            return {
+
+                success:
+                    true,
+
+                email:
+                    demoEmail,
+
+                password:
+                    demoPassword,
+
+                uid:
+                    user.uid,
+
+                libraryId:
+                    libraryId
+
+            };
+
+        }
+        catch (error) {
+
+            console.error(
+                "[Adhyayn] Demo Admin setup error:",
+                error
+            );
+
+
+            throw new functions.https.HttpsError(
+                "internal",
+                "Unable to create Demo Admin."
+            );
+
+        }
+
+    }
+);
